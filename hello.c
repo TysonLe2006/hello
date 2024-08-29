9709c74fb108ceeb57e127fa47378fe26d833dbd
Tyson Le <tle56@huskers.unl.edu>
2024-08-29 12:48:39 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Tyson Le
+ *         tle56@huskers.unl.edu
+ * Date: 2024/29/08
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+  printf("Tyson Le\n");
+
+  return 0;
+}
